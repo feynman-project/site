8a816a4850eca04eefec3bfd48bcee3033fb0265
Matt Gleich <git@mattglei.ch>
2022-01-24 17:55:00 -0500

    feat: basic elements
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,39 @@
-<h1>Feynman Project</h1>
+<script>
+	import Logo from '../components/Logo.svelte';
+</script>
+
+<main>
+	<div class="left">
+		<Logo />
+	</div>
+	<div class="right">
+		<h1>Feynman Project</h1>
+		<p>
+			<a href="https://en.wikipedia.org/wiki/Richard_Feynman">Richard Feynman</a> is one of the most
+			influential and important physicists of the last century. Outside of his innovative research in
+			the world of quantum mechanics, he was also an amazing teacher, giving his students a deep and
+			natural understanding of physics. This project aims to create physics teaching material to give
+			students a deeper understanding of physics at the high school level.
+		</p>
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0 20px;
+	}
+
+	.left {
+		padding-right: 30px;
+	}
+
+	h1 {
+		margin-top: 15px;
+	}
+
+	p {
+		max-width: 600px;
+	}
+</style>
